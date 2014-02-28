7629965a1b338821ca3d790e35bd16fa97b8be66
Eric Lambrecht <eml@ill.com>
2014-02-27 19:48:44 -0800

    first pass of working ajax call

--- Assets/Scripts/NetworkRequest.cs
+++ Assets/Scripts/NetworkRequest.cs
@@ -6,71 +6,160 @@ using System.Collections.Generic;
 
 public class NetworkRequest : MonoBehaviour {
 
-	private delegate void SuccessResponse(WWW request, JSONNode response);
-	private delegate void ErrorResponse(WWW request);
+	private delegate void WWWResponse(WWW request, JSONNode response);
 
 	private enum RequestType { GET, POST };
 
 	// 'POST' call with arguments
-	private void Ajax(RequestType requestType, string url, Dictionary<string, string> fields, SuccessResponse success, ErrorResponse error) {
+	private void Ajax(RequestType requestType, string url, Dictionary<string, string> fields, WWWResponse success, WWWResponse error) {
 		StartCoroutine(AjaxRoutine(requestType, url, fields, success, error));
 	}
 
 	// 'GET' call
-	private void Ajax(string url, SuccessResponse success, ErrorResponse error) {
+	private void Ajax(string url, 
+	                  WWWResponse success, 
+	                  WWWResponse error) {
 		StartCoroutine (AjaxRoutine(RequestType.GET, url, null, success, error));
 	}
 
-	private IEnumerator AjaxRoutine(RequestType requestType, string url, Dictionary<string, string> fields, SuccessResponse success, ErrorResponse error) {
+	private IEnumerator AjaxRoutine(RequestType requestType, 
+	                                string url, 
+	                                Dictionary<string, string> fields, 
+	                                WWWResponse success, 
+	                                WWWResponse error) {
 		WWW www;
+		WWWForm form = new WWWForm();
 
-		if ((requestType == RequestType.POST) || (fields != null)) {
-			// note that technically this doesn't support a GET with parameters. we'll cross that bridge
-			// when we come to it.
-			WWWForm form = new WWWForm();
+		// add authentication headers
+		var headers = form.headers;
+		headers["Authorization"]="Basic " + System.Convert.ToBase64String(
+			System.Text.Encoding.UTF8.GetBytes("ac6a67377d4f3b655b6aa9ad456d25a29706355e:13b558028fc5d244c8dc1a6cc51ba091afb0be02"));
 
+		// Unity doesn't give us contents on non-200 response
+		if (fields == null) {
+			fields = new Dictionary<string, string>();
+		}		
+		fields["force200"] = "1";
+
+
+		if (requestType == RequestType.POST) {
 			if (fields != null) {
-				foreach (string key in fields.Keys) {
-					form.AddField(key, fields[key]);
+				foreach (KeyValuePair<string, string> pair in fields) {
+					form.AddField(pair.Key, pair.Value);
 				}
-			} else {
-				form.AddField("junk", "placeholder");
+
+				// note that we're assuming there is at least one entry in
+				// the dictionary, otherwise this call will fail
 			}
 
-			www = new WWW(url, form);
+			www = new WWW(url, form.data, headers);
 
 		} else {
-			www = new WWW(url);
+			if (fields != null) {
+				url = url + ToQueryString(fields);
+			}
+
+			www = new WWW(url, null, headers);
 
 		}
 
 		yield return www;
 
-		if (www.error != null) {
-			error(www);
-		
-		} else {
+		if (String.IsNullOrEmpty(www.error)) {
 			try {
 				// all responses should be JSON
 				JSONNode jsonResponse = JSONNode.Parse(www.text);
 
-				success(www, jsonResponse);
+				if (jsonResponse["success"].AsBool) {
+					success(www, jsonResponse);
+				} else {
+					Debug.Log ("success = false");
+					error(www, jsonResponse);
+				}
 
 			} catch (Exception) {
-				error(www);
+				Debug.Log ("response is not parseable");
+				error(www, null);
 			}			
+
+		} else if (www.error != null) {
+			Debug.Log ("byte count is " + www.bytesDownloaded);
+			Debug.Log ("bytes are " + System.Text.Encoding.Default.GetString (www.bytes));
+			
+			try {
+				JSONNode jsonResponse = JSONNode.Parse (www.text);
+				
+				error(www, jsonResponse);
+
+			} catch (Exception) {
+				error(www, null);
+
+			}
+
+		} else {
+			Debug.Log ("byte count is " + www.bytesDownloaded);
+			Debug.Log ("bytes are " + System.Text.Encoding.Default.GetString (www.bytes));
+
+			error(www, null);
 		}
+	}
+
+	private string ToQueryString(Dictionary<string, string> nvc)
+	{
+		string query = "";
+
+		foreach (KeyValuePair<string, string> pair in nvc) {
+			if (query.Length > 0) {
+				query += "&";
+			} else {
+				query += "?";
+			}
+
+			query += string.Format("{0}={1}", WWW.EscapeURL(pair.Key), WWW.EscapeURL(pair.Value));
+		}
+
+		return query;
 	}
 
 	// Use this for initialization
 	void Start () {
+		/*
 		Ajax("https://feed.fm/api/v2/oauth/time", 
-	        	((WWW r, JSONNode d) => Debug.Log("success! time is " + d["time"])), 
-                ((WWW r) => Debug.Log("error!")));
+        	 ((WWW r, JSONNode d) => Debug.Log("success! time is " + d["time"])), 
+        	 ((WWW r) => Debug.Log("error!")));		
+        	 */
+		RequestClientId ("http://feed.localdomain:8000/missing");
 
-		Ajax (RequestType.POST, "https://feed.fm/api/v2/client", null,
-		      ((WWW r, JSONNode d) => Debug.Log ("got a client id: " + d["client_id"])),
-		      ((WWW r) => Debug.Log("error getting client id :" + r.text)));
 	}
-	
+
+	private void RequestClientId(string url) {
+		Ajax (RequestType.POST, 
+		      //"https://feed.fm/api/v2/client", 
+		      url,
+		      null, 
+		      OnClientSuccess,
+		      OnClientError);
+	}
+
+	private void OnClientSuccess(WWW r, JSONNode d) {
+		Debug.Log ("found a new client id " + d["client_id"]);
+	}
+
+
+	private void OnClientError(WWW r, JSONNode d) {
+		WaitAndThen(2.0f, () => {
+			RequestClientId ("http://feed.localdomain:8000/missing");
+		});
+	}	
+
+	private void WaitAndThen(float time, Action action) {
+		StartCoroutine (WaitAndThenCoRoutine(time, action));
+	}
+
+	private IEnumerator WaitAndThenCoRoutine(float time, Action action) {
+		yield return new WaitForSeconds(time);
+
+		action();
+	}
+
 }
